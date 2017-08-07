2f64b2a37ef3fa6d98a82a2772ad11ace6351baa
jarrymeng <jarrymeng@aliyun.com>
2017-08-07 23:31:18 +0800

    mod

--- src/views/dataCount/DataCount.vue
+++ src/views/dataCount/DataCount.vue
@@ -3,102 +3,124 @@
     width:100%;
     height:100%;
     background:#1a2742;
-    padding: 0 50px;
+    padding: 0 50px 40px;
+
+    overflow-y: scroll;
     .mesHead{
+      margin: 40px 0 42px 0;
+      .weather{
+        height:100%;
 
-    }
-    .contentRow{
-      &>div{
-        height:214px;
-        font-size: 20px;
-        color:#b8c6e8;
-        background: #2a3b60;
-        h3{
+        .img{
 
-          line-height: 52px;
-          font-weight: normal;
-        }
-        .streetCount{
-          line-height: 64px;
-          display: flex;
-          flex-direction: row;
-          p{
-            flex:1;
+          position: relative;
+          width:100%;
+          padding-bottom: 33%;
+          img{
+            width:100%;
+            height:100%;
+            position: absolute;
+
           }
         }
-        .progress{
-          line-height: 34px;
+
+      }
+      .count{
+        margin-left:38px;
+        padding-right: 38px;
+        p{
+          font-size: 22px;
+          line-height: 58px;
+          color:#b8c6e8;
+          span{
+            color:#3e9bfe;
+            font-size: 34px;
+            font-weight: bold;
+          }
+        }
+
+
+      }
+    }
+    .contentRow{
+      margin: 0 -4px;
+      .item_wrapper{
+        &>div{
+          color:#b8c6e8;
+          background: #2a3b60;
+          font-size: 18px;
+          padding-left: 24px;
+          padding-bottom: 28px;
+          margin:4px;
+          h3{
+
+            line-height: 52px;
+            font-weight: normal;
+          }
+          .streetCount{
+            line-height: 64px;
+            p{
+              float: left;
+              width: 50%;
+              span{
+                color:#fff;
+                font-size: 16px;
+              }
+            }
+          }
+          .progress{
+            line-height: 34px;
+          }
+          .ivu-progress-show-info .ivu-progress-outer {
+              padding-right: 42px;
+          }
+          .ivu-progress-inner {
+              border-radius:0;
+          }
+          .ivu-progress-bg {
+              border-radius: 0;
+          }
+          .ivu-progress-text {
+              margin-left: 15px;
+          }
         }
       }
+
     }
   }
 </style>
 <template lang="html">
   <div class="dataContent">
     <Row class="mesHead">
-        <Col class="weather">
+        <Col span="6" class="weather">
+          <div class="img">
+            <img src="../../img/n_33.png" alt="">
+          </div>
 
         </Col>
-        <Col class="count">
+        <Col span="5" class="count">
           <div>
-            <p>常住户数<span>25681</span></p>
-            <p>商户饭店<span>15389</span></p>
+            <p>常住户数：<span>25681</span></p>
+            <p>商户饭店：<span>15389</span></p>
           </div>
         </Col>
-        <Col class="chart">
+        <Col span="13" class="chart">
           <line-chart></line-chart>
         </Col>
     </Row>
     <Row class="contentRow">
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="streetCount">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
+        <Col :sm="12" :md="8" :lg="6" class="item_wrapper" v-for="x in 12">
+          <div>
+            <h3>秦虹街道</h3>
+            <div class="streetCount">
+              <p>常住户数:<span>15873</span></p>
+              <p>商户饭店:<span>1873</span></p>
+            </div>
+            <p class="progress">燃气报警次数</p>
+            <Progress :percent="45" :stroke-width="16" status="active"><span>45</span></Progress>
           </div>
-          <p class="progress">燃气报警次数</p>
-          <Progress :percent="45" status="active"></Progress>
         </Col>
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
-          </div>
-          <Progress :percent="45" status="active"></Progress>
-        </Col>
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
-          </div>
-          <Progress :percent="45" status="active"></Progress>
-        </Col>
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
-          </div>
-          <Progress :percent="45" status="active"></Progress>
-        </Col>
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
-          </div>
-          <Progress :percent="45" status="active"></Progress>
-        </Col>
-        <Col span="6">
-          <h3>秦虹街道</h3>
-          <div class="">
-            <p>常住户数:<span>15873</span></p>
-            <p>商户饭店:<span>1873</span></p>
-          </div>
-          <Progress :percent="45" status="active"></Progress>
-        </Col>
+
     </Row>
   </div>
 </template>
