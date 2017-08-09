8eda7355fc7a79f005f5d307a32cfe39893c16a5
Mengzeyue <jarrymeng@aliyun.com>
2017-08-09 16:40:28 +0800

    mod

--- src/views/map/Map.vue
+++ src/views/map/Map.vue
@@ -9,7 +9,7 @@
     /* chrome, firefox */
     url('../../iconfont/mapIconfont.ttf?t=1501226686963') format('truetype'),
     /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
-    url( '../../iconfont/mapIconfont.svg?t=1501226686963#iconfont') format('svg');
+    url(  '../../iconfont/mapIconfont.svg?t=1501226686963#iconfont') format('svg');
     /* iOS 4.1- */
 }
 .icon-dingwei:before {
@@ -35,16 +35,16 @@
         height: 100%;
         width: 100%;
     }
-    .BMap_Marker{
-      &>div{
-        width:100%;
-        height:100%;
-        img{
-          display: block;
-          width:100%;
-          height:100%;
+    .BMap_Marker {
+        & > div {
+            width: 100%;
+            height: 100%;
+            img {
+                display: block;
+                width: 100%;
+                height: 100%;
+            }
         }
-      }
     }
     .BMap_pop {
         div {
@@ -324,11 +324,42 @@
             }
         }
     }
+    .alarmsList{
+      width:100%;
+      position: absolute;
+      bottom:0;
+      z-index: 1000;
+      background: #fff;
+      // border:1px solid #333;
+      &>ul{
 
+        &>li{
+          display: inline-block;
+          padding:5px;
+          cursor: pointer;
+          border:1px solid #333;
+          &.active{
+            background:#f00;
+            color:#fff;
+          }
+        }
+      }
+
+    }
 }
 </style>
 <template lang="html">
   <div class="map">
+    <div class="alarmsList">
+      <ul v-show="markersDataList.length>1?true:false">
+        <li v-for="items in markersDataList" @click="clickOpenInfo(items)" :class="{active:items.isWarn?true:false||items.marker.isWarn?true:false}">
+          <p><i class="ivu-icon icon-user"></i><b>户 主</b>：<span>{{items.name}}</span></p>
+          <p><i class="ivu-icon icon-device"></i><b>设 备 名 称</b>：<span>{{items.nickname}}</span></p>
+          <p><i class="ivu-icon icon-dingwei"></i><b>地 址</b>：<span>{{items.address}}</span></p>
+
+        </li>
+      </ul>
+    </div>
     <div id="map"></div>
   </div>
 </template>
@@ -338,10 +369,11 @@ export default {
   name: 'map',
   data() {
     return {
-      pointData: [],
+      pointData: [], //点 的数据
       street: [],
-      markers:[],
-      rangePoint:[],
+      markers: [], //点列表
+      rangePoint: [], //点击之后在圆圈范围的点
+      markersDataList: [], //点击之后在圆圈范围的点的数据
       opts: {
         width: 640, // 信息窗口宽度
         height: 400, // 信息窗口高度
@@ -353,14 +385,14 @@ export default {
 
   mounted() {
     // 百度地图API功能
-    console.log(this.street)
+    // console.log(this.street)
 
     var map = new BMap.Map("map", {
       enableMapClick: false
     }); // 创建Map实例
-    // map.centerAndZoom(new BMap.Point(118.823513,32.020812),14);  // 初始化地图,设置中心点坐标和地图级别
-    // map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-    // map.setCurrentCity("南京");          // 设置地图显示的城市 此项是必须设置的
+    map.centerAndZoom(new BMap.Point(118.823513,32.020812),14);  // 初始化地图,设置中心点坐标和地图级别
+    map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+    map.setCurrentCity("南京");          // 设置地图显示的城市 此项是必须设置的
     map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
     map.setMapStyle({ //设置地图样式
       styleJson: [{
@@ -384,7 +416,7 @@ export default {
         }
       ]
     });
-    this.getBoundary(map);
+    // this.getBoundary(map);
     this.makePoint(map);
     map.addEventListener("tilesloaded", function() { //地图加载完成触发的函数
 
@@ -425,11 +457,11 @@ export default {
           this.pointData.forEach((item, index) => {
 
             this.axios('http://58.213.47.166:8990/device/belong?did=' + item.id) //
-            .then(resp => {
-              // console.log(resp.data.belong)
-              this.$set(this.pointData[index], 'name', resp.data.belong.name);
-              this.$set(this.pointData[index], 'tel', resp.data.belong.tel);
-            })
+              .then(resp => {
+                // console.log(resp.data.belong)
+                this.$set(this.pointData[index], 'name', resp.data.belong.name);
+                this.$set(this.pointData[index], 'tel', resp.data.belong.tel);
+              })
 
             for (let i = 0; i < this.street.length; i++) { //获取完整设备地址
               if (item.sid == this.street[i].sid) {
@@ -448,6 +480,7 @@ export default {
               icon: myIcon
             }); // 创建标注
             // this.markers.push(pt)
+            marker.did = item.id;
             this.markers.push(marker);
             marker.setTitle(item.address);
             map.addOverlay(marker);
@@ -458,18 +491,20 @@ export default {
             this.watchPoint(map, item, marker)
 
           })
-          console.log(map)
-          console.log(this.markers)
+          // console.log(map)
+          // console.log(this.markers)
           // this.markerData = new BMapLib.MarkerClusterer(map, {markers:this.markers});
         })
 
     },
-    watchPoint(map, item, marker) {     //设备点监听
+    watchPoint(map, item, marker) { //设备点监听
       clearInterval(marker.setInt)
+
       marker.setInt = setInterval(function() {
+        // console.log(marker)
         this.axios('http://service.wanwuyun.com:8920/devicedata/' + item.seckey + '?count=1')
-          .then(function(res) {
-            if(res.data.data.length>=1){
+          .then((res)=> {
+            if (res.data.data.length >= 1) {
               res.data.data[0].ALARM = '2'
               if (res.data.data[0].ALARM == '2') {
                 // console.log(marker)
@@ -479,16 +514,23 @@ export default {
                   let timeDiff = nowDate - marker.infoCreateTime
 
                   if (timeDiff > 30000) { //设定多少分钟后   依然报警  再次跳动（毫秒）  1秒=1000毫秒
-                    marker.isWarn = true;
+                    this.$set(marker,'isWarn',true)
+                    // marker.isWarn = true;
                     marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
                     let myIcon = new BMap.Icon("./img/marker2.png",
                       new BMap.Size(39, 42), {});
                     marker.setIcon(myIcon);
-                  }else{
-
+                  } else {
+                    // console.log(marker.isWarn)
+                    // if(marker.isWarn){
+                    //   this.$set(marker,'isWarn',false)
+                    //   // marker.isWarn=false
+                    //   console.log("那啥啥%o",marker)
+                    // }
                   }
                 } else {
-                  marker.isWarn = true;
+                  this.$set(marker,'isWarn',true)
+                  // marker.isWarn = true;
                   marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
                   let myIcon = new BMap.Icon("./img/marker2.png",
                     new BMap.Size(39, 42), {});
@@ -497,16 +539,92 @@ export default {
 
 
               }
-            }else{
-
+            } else {
+              // console.log(marker.isWarn)
+              // if(marker.isWarn){
+              //   this.$set(marker,'isWarn',false)
+              //   // marker.isWarn=false
+              //   console.log("那啥啥%o",marker)
+              // }
             }
 
           })
       }.bind(this), 2500)
 
     },
-    addClickHandler(map, item, marker) {   //设备点弹出框
-      let that = this;
+    addClickHandler(map, item, marker) { //设备点弹出框
+      // let that = this;
+
+
+      marker.addEventListener("click", function(e) {
+        // console.log(marker)
+        marker.setAnimation(null);
+
+        // let myIcon = new BMap.Icon("./img/marker2.png",
+        //   new BMap.Size(44, 49), {});
+        // marker.setIcon(myIcon);
+        this.geoUtils(marker, map, e) //判断点是否再圆内
+        if(this.rangePoint.length<=1){
+          clearInterval(marker.setInt)
+          this.openInfo(marker,item, map) //打开弹出框
+          marker.infoCreateTime = new Date().getTime();
+          this.watchPoint(map, item, marker) //监听每个点是否报警
+        }
+
+
+
+
+
+        /////////////////////////////////////////
+
+        // console.log('范围点数组:%o', this.rangePoint)
+        // console.log('范围点的数据:%o', this.markersDataList)
+      }.bind(this));
+
+    },
+    geoUtils(marker, map, content, e) {
+
+      // new Promise(()=>{
+      this.rangePoint = [];
+      this.markersDataList = []
+      var c = new BMap.Point(marker.point.lng, marker.point.lat); //圆心
+      var circle = new BMap.Circle(c, 50); //范围圆
+      for (var i = 0; i < this.markers.length; i++) {
+        var pt = new BMap.Point(this.markers[i].point.lng, this.markers[i].point.lat); //每个点
+        var result = BMapLib.GeoUtils.isPointInCircle(pt, circle);
+        if (result == true) {
+          for (var j = 0; j < this.pointData.length; j++) {
+            if (this.markers[i].did == this.pointData[j].id) {
+              this.$set(this.pointData[j],'isWarn',this.markers[i].isWarn||false)
+              this.$set(this.pointData[j],'marker',this.markers[i])
+              this.$set(this.pointData[j],'map',map)
+              this.markersDataList.push(this.pointData[j])
+            }
+          }
+          this.rangePoint.push(this.markers[i]);
+        } else {
+          // console.log("点在圆形外")
+        }
+      }
+      // }).then(()=>{
+      //   // for(let i=0;i<)
+      // })
+      // console.log(this.rangePoint)
+
+      // map.addOverlay(circle);  //演示：将点与圆形添加到地图上
+    },
+    clickOpenInfo(items){
+      // items.map.clearOverlays(); //清除地图覆盖物
+      this.$set(items,'isWarn',false)
+      // items.isWarn=false;
+      items.marker.setAnimation(null);
+      clearInterval(items.marker.setInt)
+      this.openInfo(items.marker,items, items.map,true)
+      items.marker.infoCreateTime = new Date().getTime();
+      this.watchPoint(items.map, items, items.marker) //监听每个点是否报警
+    },
+    openInfo(marker,item, map,s) {
+      // console.log(item.name)
       let content = '';
       let seccon = '';
       new Promise((reslove) => {
@@ -537,107 +655,72 @@ export default {
           '</div>' +
           '</div>';
         content += fircon + seccon + thrcon;
+        // console.log('窗口打开')
+        if (this.rangePoint.length <= 1||s) { //判断有一个点就直接显示出来弹框
+          // let p = e.target;
+          // marker.
+          let point = new BMap.Point(marker.point.lng, marker.point.lat);
+          let infoWindow = new BMap.InfoWindow(content, this.opts); // 创建信息窗口对象
+
+          infoWindow.addEventListener("close", function(type) {
+
+            // console.log('关闭了');
+            // console.log(marker);
+            let myIcon = new BMap.Icon("./img/marker1.png",
+              new BMap.Size(39, 42), {});
+            marker.setIcon(myIcon);
+          }.bind(this));
+
+          new Promise((reslove) => {
+            map.openInfoWindow(infoWindow, point); //开启信息窗口
+
+            reslove()
+          }).then(() => {
+            setTimeout(()=> {
+              // console.log(marker.isWarn)
+              let bMapPop = document.getElementsByClassName('BMap_pop')[0]
+              // console.log(bMapPop.className)
+              //
+              // console.log(marker)
+              if (marker.isWarn) {
+                addClass(bMapPop, 'active')
+              } else {
+                removeClass(bMapPop, 'active')
+              }
+              //你妹的 :-(   改良的版本
+              function addClass(obj, cls) {
+                let obj_class = obj.className; //获取 class 内容.
+                let blank = (obj_class != '') ? ' ' : ''; //判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
+                let added = obj_class + blank + cls; //组合原来的 class 和需要添加的 class.
+
+                let classArr = added.split(' ');
+                //class去重 不重复添加    解决：多个报警点切换  class添加多个  会出现报警样式不变的情况bug   ps:或者更改移除class 查找有没有多个  全部移除
+                let newClassArr = [];
+                for (let i = 0; i < classArr.length; i++) {
+                  if (newClassArr.indexOf(classArr[i]) == -1) {
+                    newClassArr.push(classArr[i])
+                  }
+                }
+                obj.className = newClassArr.join(' '); //替换原来的 class.
+              }
+
+              function removeClass(obj, cls) {
+                let obj_class = ' ' + obj.className + ' '; //获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
+                obj_class = obj_class.replace(/(\s+)/gi, ' ') //将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
+                let removed = obj_class.replace(' ' + cls + ' ', ' '); //在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
+                removed = removed.replace(/(^\s+)|(\s+$)/g, ''); //去掉首尾空格. ex) 'bcd ' -> 'bcd'
+                obj.className = removed; //替换原来的 class.
+              }
+              this.$set(marker,'isWarn',false)
+              // marker.isWarn = false;
+
+            })
+          })
+        } else { //多个点列表显示
+
+        }
       })
 
-      marker.addEventListener("click", function(e) {
-        console.log(marker)
-        marker.setAnimation(null);
-        clearInterval(marker.setInt)
-        // let myIcon = new BMap.Icon("./img/marker2.png",
-        //   new BMap.Size(44, 49), {});
-        // marker.setIcon(myIcon);
-        this.geoUtils(map,marker)//判断点是否再圆内
-        this.openInfo(marker, map, content, e)//打开弹出框
-
-        marker.infoCreateTime = new Date().getTime();
-        this.watchPoint(map, item, marker) //监听每个点是否报警
-
-        /////////////////////////////////////////
-
-
-      }.bind(this));
-
-    },
-    geoUtils(map,marker){
-      this.rangePoint=[];
-      var c = new BMap.Point(marker.point.lng, marker.point.lat); //圆心
-      var circle = new BMap.Circle(c, 50);//范围圆
-      for(let i=0;i<this.markers.length;i++){
-        var pt = new BMap.Point(this.markers[i].point.lng, this.markers[i].point.lat);//每个点
-        var result = BMapLib.GeoUtils.isPointInCircle(pt, circle);
-        if(result == true){
-            this.rangePoint.push(this.markers[i])
-        } else {
-            console.log("点在圆形外")
-        }
-      }
-      // console.log(this.rangePoint)
-
-      // //演示：将点与圆形添加到地图上
-
-      map.addOverlay(circle);
-    },
-    openInfo(marker, map, content, e) {
-      console.log('范围点数组:%o',this.rangePoint)
-      if(this.rangePoint.length<=1){ //判断有一个点就直接显示出来弹框
-        let p = e.target;
-        let point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
-        let infoWindow = new BMap.InfoWindow(content, this.opts); // 创建信息窗口对象
-
-        infoWindow.addEventListener("close", function(type) {
-          console.log('关闭了');
-          console.log(marker);
-          let myIcon = new BMap.Icon("./img/marker1.png",
-            new BMap.Size(39, 42), {});
-          marker.setIcon(myIcon);
-        }.bind(this));
-
-        new Promise((reslove) => {
-          map.openInfoWindow(infoWindow, point); //开启信息窗口
-
-          reslove()
-        }).then(() => {
-          setTimeout(function() {
-            // console.log(marker.isWarn)
-            let bMapPop = document.getElementsByClassName('BMap_pop')[0]
-            console.log(bMapPop.className)
-
-            console.log(bMapPop)
-            if (marker.isWarn) {
-              addClass(bMapPop, 'active')
-            } else {
-              removeClass(bMapPop, 'active')
-            }
-            //你妹的 :-(   改良的版本
-            function addClass(obj, cls) {
-              let obj_class = obj.className; //获取 class 内容.
-              let blank = (obj_class != '') ? ' ' : ''; //判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
-              let added = obj_class + blank + cls; //组合原来的 class 和需要添加的 class.
-
-              let classArr = added.split(' ');
-              //class去重 不重复添加    解决：多个报警点切换  class添加多个  会出现报警样式不变的情况bug   ps:或者更改移除class 查找有没有多个  全部移除
-              let newClassArr = [];
-              for (let i = 0; i < classArr.length; i++) {
-                if (newClassArr.indexOf(classArr[i]) == -1) {
-                  newClassArr.push(classArr[i])
-                }
-              }
-              obj.className = newClassArr.join(' '); //替换原来的 class.
-            }
-
-            function removeClass(obj, cls) {
-              let obj_class = ' ' + obj.className + ' '; //获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
-              obj_class = obj_class.replace(/(\s+)/gi, ' ') //将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
-              let removed = obj_class.replace(' ' + cls + ' ', ' '); //在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
-              removed = removed.replace(/(^\s+)|(\s+$)/g, ''); //去掉首尾空格. ex) 'bcd ' -> 'bcd'
-              obj.className = removed; //替换原来的 class.
-            }
-            marker.isWarn = false;
-          })
-        })
-      }else{ //多个点列表显示
-
-      }
 
 
 
@@ -647,9 +730,9 @@ export default {
   },
   created() {
     this.axios.get('http://58.213.47.166:8990/area/street?aid=2086')
-    .then(res => {
-      this.street = res.data
-    })
+      .then(res => {
+        this.street = res.data
+      })
   }
 }
 </script>
